29fe76d56c1e4e9670657f369b081808d001100d
RaviJain2803 <jravi1101@gmail.com>
2026-07-09 10:43:30 +0530

    Update CORS to allow localhost and production frontend

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,10 @@ import Gateway from "./controllers/paymentController.js";
 // app.use(cors());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,
+    ],
     credentials: true,
   }),
 );
